7737f142b2838b3de1f951a081de46500c5dca8a
Guillermo Bernal <gbernal096@hotmail.com>
2015-09-02 12:55:26 -0500

    THIS GONNA BE PAINFUL - Agenda agenda agenda...

---
+++ app/src/main/java/toa/toa/agenda/AgendaMan.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright TOA Inc. 2015.
+ */
+
+package toa.toa.agenda;
+
+/**
+ * Created by gbern on 8/13/2015.
+ */
+public class AgendaMan {
+
+    public static void saveEvent(int id) {
+
+    }
+
+
+    public interface AgendaCols {
+        String NAME = "name";
+        String DATESTART = "dates";
+        String DATEEND = "datee";
+        String RELID = "rel_id";
+    }
+
+
+}
